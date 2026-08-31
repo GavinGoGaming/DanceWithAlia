f11b4a9fe7039b9b5bf157ebd0080c1aaa61a5a8
Gavin Fox <gavingogaming@gmail.com>
2026-08-30 20:59:34 -0700

    Button larger

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 const featuredVideos = [
     {
-        url: "https://www.youtube.com/watch?v=_ECosT3rFTw",
-        title: "Interview with Illan and Mouna at Tribal Fest 2012",
-        thumbnail: "https://img.youtube.com/vi/_ECosT3rFTw/hqdefault.jpg"
+        url: "https://www.youtube.com/watch?v=XeCazZV1PLs",
+        title: "Pasadena Greek Festival 2013",
+        thumbnail: "https://img.youtube.com/vi/XeCazZV1PLs/hqdefault.jpg"
     },
     {
         url: "https://www.youtube.com/watch?v=FC2CaJXrzCg",
@@ -23,10 +23,19 @@ export default function Home() {
                 <section id="header" className="header-section">
                     <img src="/logo.png" alt="Dance With Alia" className="logo" />
                     <div className="header-buttons">
-                        <a href="https://www.youtube.com/dancewithalia"><i className="fab fa-youtube"></i></a>
-                        <a href="https://www.instagram.com/dance_with_alia/"><i className="fab fa-instagram"></i></a>
+                        <a href="https://www.youtube.com/dancewithalia" className="youtube">
+                            <i className="fab fa-youtube"></i>
+                            <span>Watch on YouTube</span>
+                        </a>
+                        <a href="https://www.instagram.com/dance_with_alia/" className="instagram">
+                            <i className="fab fa-instagram"></i>
+                            <span>Follow on Instagram</span>
+                        </a>
                         {/* <a href="https://x.com/dancewithalia/"><i className="fab fa-x-twitter"></i></a> */}
                     </div>
+                </section>
+                <section id="header-image" className="header-image-section">
+                    <img src="/exploring.png" alt="Exploring the world, one dance at a time." />
                 </section>
                 <div className="section-divider"></div>
                 <section id="graphic" className="graphic-section">
@@ -38,7 +47,7 @@ export default function Home() {
                 <section id="about" className="about-section">
                     <img src="/profile.jpg" alt="Profile Picture" />
                     <div className="about-content">
-                        <span className="title">Alia Bartlett</span>
+                        {/* <span className="title">Alia Bartlett</span> */}
                         <p className="description">Alia explores dance and music around the world and interviews people who create and sustain historical legacies.
 <br/><br/>There is so much to discover and I welcome your suggestions and contributions.</p>
                     </div>
@@ -56,7 +65,6 @@ export default function Home() {
                 </div>
                 <div className="section-divider"></div>
                 <div id="footer" className="footer-section">
-                    <img src="/exploring.png" alt="Exploring the world, one dance at a time." />
                     <span className="copyright">Copyright @ DanceWithAlia.com. All Rights Reserved.</span>
                 </div>
             </main>
